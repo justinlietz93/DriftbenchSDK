0501e5e3ad8ae19e50b97bd2b37000ce1a61310c
Justin Lietz <156801407+justinlietz93@users.noreply.github.com>
2025-05-02 22:05:41 -0500

    chore: remove scaffold files and initial project structure

--- sdk/utils.py
+++
@@ -1,8 +0,0 @@
-"""Utility functions for the DriftBench SDK."""
-
-# TODO: Add common helper functions here as needed.
-def example_utility_function():
-    """An example utility function."""
-    print("TODO: Implement utility functions")
-    return True
-
--- sdk/validators/__init__.py
+++
@@ -1,1 +0,0 @@
-# This file makes the 'validators' directory a Python package.
